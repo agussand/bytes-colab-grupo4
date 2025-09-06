b0b0cb63d8e9ec1b096cccd969934b1aa1d70bfc
MarvinAchas <marvinacha@ghlperu.com>
2025-09-05 22:55:12 -0500

    fix: actualizando nombre de los endpoints

--- src/main/java/com/bytescolab/demo/featureflagapi/configAuth/SecurityConfiguration.java
+++ src/main/java/com/bytescolab/demo/featureflagapi/configAuth/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**")
+                        .requestMatchers("/api/auth/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
