30dffe50a1ce9bbd276d56958b23cdca6d705c27
agussand <agus.sand@hotmail.com>
2025-09-08 00:22:00 -0300

    fix: jwt secret desde .env y fix dto

--- src/main/java/com/bytescolab/demo/featureflagapi/controller/FeatureController.java
+++ src/main/java/com/bytescolab/demo/featureflagapi/controller/FeatureController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class FeatureController {
 
     @PostMapping
     public ResponseEntity<Feature> save(
-            @Valid FeaturePOSTDTO newFeature
+            @Valid @RequestBody FeaturePOSTDTO newFeature
             ){
         return ResponseEntity.ok(featureService.save(newFeature));
     }
--- src/main/java/com/bytescolab/demo/featureflagapi/dto/FeaturePOSTDTO.java
+++ src/main/java/com/bytescolab/demo/featureflagapi/dto/FeaturePOSTDTO.java
@@ -1,6 +1,7 @@
 package com.bytescolab.demo.featureflagapi.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +13,8 @@ import lombok.NoArgsConstructor;
 public class FeaturePOSTDTO {
     //agregar que sea único
     private String name;
-    @NotBlank
+    @NotBlank(message = "description no puede estar vacío")
+    @JsonProperty("description")
     private String description;
     private Boolean enabledByDefault;
 }
